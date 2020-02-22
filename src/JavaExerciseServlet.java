0608272da7a9da152ea0944a24535465d3554404
Lars Schweisthal <lars.schweisthal@gmail.com>
2020-02-22 16:54:28 +0100

    Added bare servlet class

---
+++ src/JavaExerciseServlet.java
@@ -0,0 +1,14 @@
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class JavaExerciseServlet extends javax.servlet.http.HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<h1>Ich bin ein Servlet</h1>");
+    }
+}
